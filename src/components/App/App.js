9a5d8b4f62b9719ea45cdf723c5c60082c4d6bdb
Paulina Babinczuk <paulina.babinczuk@gmail.com>
2020-12-21 16:04:07 +0100

    add search

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './App.scss';
 import List from '../List/ListContainer.js';
 import PropTypes from 'prop-types';
+import Search from '../Search/SearchContainer';
 
 
 class App extends React.Component {
@@ -12,11 +13,12 @@ class App extends React.Component {
   }
 
   render() {
-    const {lists, title, subtitle} = this.props;
+    const { lists, title, subtitle } = this.props;
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
+        <Search/>
         {lists.map(listData => (
           <List key={listData.id} {...listData} />
         ))}
